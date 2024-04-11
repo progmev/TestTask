e8237da9e32d38122367a3402438b6ae4cd234b7
EVGENY MARTYNOV <progmev@gmail.com>
2024-04-11 09:59:20 +0300

    Create new project

---
+++ TestTask/TestTask/TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskApp.swift
+//  TestTask
+//
+//  Created by papa on 11.04.24.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
